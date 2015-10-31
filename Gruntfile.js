da38995c078d51453ae69767b50e7db44ba13cea
Danielle Johnson <dinkydani@googlemail.com>
2015-10-31 18:26:34 +0000

    Moved static assets to assets, written grunt task, cleaned up unused css and js files

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+
+  'use strict';
+
+  grunt.initConfig({
+
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: ['Gruntfile.js', 'src/javascript/**/*.js', '!src/javascript/templates.js']
+    },
+
+    sass: {
+      dist: {
+        files: {
+          'assets/css/main.css':'scss/main.scss'
+        }
+      }
+    },
+
+    autoprefixer: {
+      no_dest: {
+        src: 'assets/css/main.css'
+      }
+    },
+
+    watch: {
+      sass: {
+        files: ['sass/**/*.scss'],
+        tasks: ['sass', 'autoprefixer']
+      },
+      jshint: {
+        files: ['js/**/*.js'],
+        tasks: ['jshint']
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('build', ['jshint', 'sass', 'autoprefixer']);
+  grunt.registerTask('default', ['jshint', 'sass', 'autoprefixer', 'watch']);
+
+};
