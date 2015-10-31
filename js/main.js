7e5989da644e2dc6e4ce40a23a0937e82027c063
Danielle Johnson <dinkydani@googlemail.com>
2015-10-31 16:50:08 +0000

    Fixed height, added testimonials

--- js/main.js
+++ js/main.js
@@ -6,22 +6,33 @@
 
   $('.c-header__select').on('change', function (e) {
     var page = $(e.currentTarget).val() - 1;
-    window.location.hash = $headerLinks.find('.c-header__link').eq(page).find('a').attr('href')
+    // window.location.hash = $headerLinks.find('.c-header__link').eq(page).find('a').attr('href')
+    window.location.href = $headerLinks.find('.c-header__link').eq(page).find('a').attr('href')
   });
 
+
+  // About Us
+  if($('.owl-carousel').length) {
+    $('.owl-carousel').owlCarousel({
+      autoPlay: true,
+      navigation: false,
+      slideSpeed : 300,
+      paginationSpeed : 500,
+      rewindSpeed: 300,
+      singleItem: true
+    });
+  }
 
   // Smart Way
   $('.smartway__links').on('click', '.smartway__link', function (e) {
     var $target = $(e.currentTarget);
     if($target.hasClass('smartway__link--active')) {
       $target.removeClass('smartway__link--active');
-    }
-    else{
+    } else {
       $('.smartway__link--active').removeClass('smartway__link--active');
       $target.toggleClass('smartway__link--active');
     }
   })
-
 
   // Register
   $('.js-register-submit').on('click', function () {
@@ -32,10 +43,13 @@
     $('.register__form').clear()
   });
 
-  L.mapbox.accessToken = 'pk.eyJ1Ijoiaml2aW5ncyIsImEiOiJ6dzhhM1FJIn0.irjChrcnF1fcbBbDLvjVUQ';
-  // Create a map in the div #map
+  // Contact Us
+  if(window.L) {
+    L.mapbox.accessToken = 'pk.eyJ1Ijoiaml2aW5ncyIsImEiOiJ6dzhhM1FJIn0.irjChrcnF1fcbBbDLvjVUQ';
+    // Create a map in the div #map
 
-  L.mapbox.map('mapbox', 'jivings.nn3l32eo')
-    .setView([51.543666, -0.17535200000000373], 13);
+    L.mapbox.map('mapbox', 'jivings.nn3l32eo')
+      .setView([51.543666, -0.17535200000000373], 13);
+  }
 
 })();
